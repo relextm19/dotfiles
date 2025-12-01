e908d7bbd3bdbf76eea8a0b98c2e7b6d557ffd34
Mateusz Kiełb <relextm19@gmail.com>
2025-12-01 22:20:31 +0100

    changed to stow for menaging dotfiles

---
+++ nvim/.config/nvim/lua/relextm19/config/keymaps.lua
@@ -0,0 +1,44 @@
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+map("n", "<leader>e", ":Oil<CR>", { desc = "Open oil"} )
+
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+map("n", "<leader>p", '"+p', { desc = "Paste from system clipboard after cursor" })
+map("n", "<leader>P", '"+P', { desc = "Paste from system clipboard before cursor" })
+
+vim.keymap.set('n', '<Esc>', ':noh<CR><Esc>', { noremap = true, silent = true })
+
+
+map("n", "<leader>k", function() vim.diagnostic.open_float() end)
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+
+    local opts = { buffer = bufnr, silent = true, noremap = true }
+
+    map("n", "gd", vim.lsp.buf.definition, opts)
+    map("n", "gD", vim.lsp.buf.declaration, opts)
+    map("n", "gr", vim.lsp.buf.references, opts)
+    map("n", "gi", vim.lsp.buf.implementation, opts)
+    map("n", "K", vim.lsp.buf.hover, opts)
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  end,
+})
+
+local telescope_opts = { noremap = true, silent = true }
+
+map("n", "<leader>FF", function() require("telescope.builtin").find_files() end, telescope_opts)
+map("n", "<leader>ff", function() require("telescope.builtin").git_files() end, telescope_opts)
+map("n", "<leader>fg", function() require("telescope.builtin").live_grep() end, telescope_opts)
+map("n", "<leader>fb", function() require("telescope.builtin").buffers() end, telescope_opts)
+map("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, telescope_opts)
+map("n", "<leader>ft", function() require("telescope.builtin").tags() end, telescope_opts)
+map("n", "<leader>fc", function() require("telescope.builtin").commands() end, telescope_opts)
+map("n", "<leader>fk", function() require("telescope.builtin").keymaps() end, telescope_opts)
---
+++ nvim/.config/nvim/lua/relextm19/core/lsp.lua
@@ -0,0 +1,50 @@
+-- Enable LSP servers
+vim.lsp.enable({ "gopls", "lua_ls", "ts_ls", "vtsls", "tailwindcss"})
+local vue_language_server_path = vim.fn.stdpath('data') .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
+}
+vim.lsp.config('vtsls', {
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                },
+            },
+        },
+    },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+})
+vim.lsp.enable('vue_ls')
+
+-- Diagnostics setup
+vim.diagnostic.config({
+    underline = true,
+    virtual_text = {
+        spacing = 4,             -- a bit of space from the code
+        source = "if_many",      -- show the source if multiple LSPs
+        format = function(diagnostic)
+            return diagnostic.message -- just the message at end of line
+        end,
+    },
+    update_in_insert = false,
+    severity_sort = true,
+    float = { border = "rounded", source = true },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
+
