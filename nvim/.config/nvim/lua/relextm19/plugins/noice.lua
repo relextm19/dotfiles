7e7fd5ac75094cbe265a47113fc77ce7b3a099a7
Mateusz Kielb <relextm19@gmail.com>
2026-01-25 14:19:24 +0100

    customized the notifications further

--- nvim/.config/nvim/lua/relextm19/plugins/noice.lua
+++ nvim/.config/nvim/lua/relextm19/plugins/noice.lua
@@ -10,6 +10,16 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-    }
+        {
+            "rcarriga/nvim-notify",
+            opts = {
+                background_colour = "#191724", -- rose pine background hardcoded so not very good but whaterver
+                render = "compact",
+                stages = "static"
+            }
+        },
+    },
+    config = function(_, opts)
+        require("noice").setup(opts)
+    end
 }
