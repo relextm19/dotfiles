3fef2a5304360c0cb371afe087db4970e37848d8
Mateusz Kielb <relextm19@gmail.com>
2026-02-23 19:13:47 +0100

    added harpoong and changed binding for git

--- nvim/.config/nvim/lua/relextm19/config/keymaps.lua
+++ nvim/.config/nvim/lua/relextm19/config/keymaps.lua
@@ -49,3 +49,18 @@ map("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, 
 map("n", "<leader>ft", function() require("telescope.builtin").tags() end, telescope_opts)
 map("n", "<leader>fc", function() require("telescope.builtin").commands() end, telescope_opts)
 map("n", "<leader>fk", function() require("telescope.builtin").keymaps() end, telescope_opts)
+
+--harpoon keybinds
+local harpoon = require("harpoon")
+harpoon:setup()
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
